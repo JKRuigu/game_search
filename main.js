bc9099d6796b3703ebba070222711567b87d5579
JKRuigu <ruigukjohn.com>
2019-12-29 15:47:16 +0300

    score update

--- main.js
+++ main.js
@@ -4,16 +4,24 @@ var player1 = 0;
 var player2 = 0;
 var current = 23;
 
+// ROW AND COLUMN VALIDATION;
+validate = (id,current)=>{
+	if (id[0] == current[0] || id[1] == current[1]) {
+		return true;
+	}	
+}
 
 //GET BUTTON NUMBER AND ADJUST SCORE;
 setKey = (e)=>{
 	if (e.srcElement.id >=11 && e.srcElement.id <=99 && e.srcElement.id != current) {
-		let value = document.getElementById(e.srcElement.id).innerHTML;
+		if (validate(String(e.srcElement.id),String(current))) {
+			let value = document.getElementById(e.srcElement.id).innerHTML;
 
-		isPlayer?player1+=Number(value):player2+=Number(value);
-		isPlayer=!isPlayer;
-		document.getElementById("player1").innerHTML = player1;
-		document.getElementById("player2").innerHTML = player2;
+			isPlayer?player1+=Number(value):player2+=Number(value);
+			isPlayer=!isPlayer;
+			document.getElementById("player1").innerHTML = player1;
+			document.getElementById("player2").innerHTML = player2;
+		}
 	}
 }
 
