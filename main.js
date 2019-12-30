061e85432328c0f849e80d17e405331bcfafd840
JKRuigu <ruigukjohn.com>
2019-12-30 15:40:00 +0300

    added engine file

--- main.js
+++ main.js
@@ -71,60 +71,7 @@ getLeft = ()=>{
 	return n;
 }
 
-// GAME CONTROLLER;
-play = id =>{
-	let value = document.getElementById(id).innerHTML;
 
-	isPlayer?player1+=Number(value):player2+=Number(value);
-	truthTable[getNumber(id)] = false;
-	let player = isPlayer?"player1":"player2";
-	if (lastPos) {
-		addClass3(current)
-	}else{
-		addClass3(current)
-	}
-	if (isPlayer) {
-		coin.play();
-	}
-
-	lastPos = current;
-	current = Number(id);
-	addClass(current,player);
-	addClass2(id);
-
-	if (isPlayer) {
-		document.getElementById("player1").innerHTML = player1;
-		document.getElementById(current).innerHTML ="P1";
-	}else{
-		document.getElementById("player2").innerHTML = player2;
-		document.getElementById(current).innerHTML ="P2";
-	}
-
-	if (getLeft() == 0) {
-		let isDraw = player1 == player2? true:false;
-		let msg = player1>player2?"ONE":"TWO";
-		if (!isDraw) {
-			mariodie.play()
-			alert(`PLAYER ${msg} WON`);
-		}else{
-			alert(`GAME ENDED AS DRAW`);			
-		}
-		initialize(current,row,column,values,ids,truthTable);
-	}
-	isPlayer=!isPlayer;
-	if (getLeft() !=0 && !isPlayer) {
-		computer(current);
-	}
-}
-
-computer = current =>{
-	// setTimeout(()=>{
-		let tempId = getMax(current);
-		console.log("COMPUTER ",tempId);
-		document.getElementById(tempId).class = "player1"
-		play(tempId);
-	// },2000)
-}
 
 //GET BUTTON NUMBER AND ADJUST SCORE;
 setKey = (e)=>{
