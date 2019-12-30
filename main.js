ac7c09ae1221e1d27043157fbc8f288221db4bf0
JKRuigu <ruigukjohn.com>
2019-12-30 15:38:25 +0300

    added validate file

--- main.js
+++ main.js
@@ -60,17 +60,6 @@ getNumber = (id)=>{
 	return (Number(String(id)[1]))-1;
 } 
 
-// console.log(window)
-
-// ROW AND COLUMN VALIDATION;
-validate = (id,current)=>{
-	if (id[0] == current[0] || id[1] == current[1]) {
-		if (truthTable[getNumber(id)]) {
-			return true;
-		}
-	}	
-}
-
 // GETS THE TOTAL UNSELECTED NUMBER IN THE GRID;
 getLeft = ()=>{
 	var n = 0;
@@ -80,39 +69,6 @@ getLeft = ()=>{
 		}
 	}
 	return n;
-}
-
-// CHECKS WHEATHER THE ROW IS FULL;
-validateRow = id =>{
-	id = Number(id);
-	var isTrue = false;
-	var data = getRow(id);
-
-	for (var i = data[0]; i <data.length; i++) {
-		if (truthTable[i]) {
-			isTrue = true;
-			break;
-		}
-	}
-
-	return isTrue;//RETURNS TRUE IS ROW IS NOT FULL;
-}
-validateRow(current);
-
-// CHECKS WHEATHER THE COLUMN IS FULL;
-validateColumn = (id)=>{
-	id = Number(String(id)[1])-1;
-	isTrue = false;
-	let data = getColumn(id);
-
-	for (var i = data[0]; i< data.length; i++) {
-		if (truthTable[i]) {
-			isTrue = true;
-			break;
-		}
-	}
-
-	return isTrue;//RETURNS TRUE IS ROW IS NOT FULL;
 }
 
 // GAME CONTROLLER;
