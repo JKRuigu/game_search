c425ff7d80ef54633c05a340e5ff8417d7ff2c97
JKRuigu <ruigukjohn.com>
2019-12-31 15:05:28 +0300

    added shuffle function

--- start.js
+++ start.js
@@ -1,15 +1,34 @@
 initialize = (current,row,column,values=[],ids=[],truthTable=[])=>{
+	let num = [1,2,3,4,5,6,7];
+
+	function shuffleWords(list) {
+	  var j, x, i;
+	  for (i = list.length - 1; i > 0; i--) {
+	    j = Math.floor(Math.random() * (i + 1));
+	    x = list[i];
+	    list[i] = list[j];
+	    list[j] = x;
+	  }
+
+	  return list;
+	}
+
+	// console.log();
+
 	// INTIALIZE GAME
 	for (var i = 1; i <= row; i++) {
+		let list = shuffleWords(num);
 		for (var j = 1; j <= column; j++) {
-			let ran = Math.floor(Math.random()*row);
-			while(ran == 0){
-				ran = Math.floor(Math.random()*row);
-			}
+			// let ran = Math.floor(Math.random()*row);
+			// while(ran == 0){
+			// 	ran = Math.floor(Math.random()*row);
+			// }
+			console.log(list[j-1]);
 			ids.push(String(i)+String(j));
-			values.push(ran);
-			document.getElementById(String(i)+String(j)).innerHTML = ran;
+			values.push(list[j-1]);
+			document.getElementById(String(i)+String(j)).innerHTML = list[j-1];
 		}
+		console.log(list);
 	}
 
 	// CREATES THE TRUTHTABLE;
